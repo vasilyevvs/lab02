803afeaf613f5c50802751af1ae35d4db93830ce
vasilyevvs <danila.vasiliev2002@yandex.ru>
2021-11-27 22:20:02 +0300

    first commit

---
+++ sources/source.cpp
@@ -0,0 +1,15 @@
+// Copyright 2020 Danila Vasilyev
+
+#include <header.hpp>
+std::string sizeConvertor(int64_t value)
+{
+  if (value <= 1024)
+    return std::to_string(value) +" b";
+  if (value >= 1024 && value < 1024*1024)
+    return std::to_string(value/1024) +" KB";
+  if (value >= 1024*1024 && value < (int64_t)1024*1024*1024)
+    return std::to_string(value/1024/1024) +" MB";
+  if (value >= (int64_t)1024*1024*1024)
+    return std::to_string(value/1024/1024/1024) +" GB";
+  return "0";
+}
